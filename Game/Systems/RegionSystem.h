0bcf103a45d7c01169e1ca57318411ed8e176ae8
dsavvinov <dsavvinov@gmail.com>
2015-11-04 01:34:15 +0300

    Added RegionSystem

---
+++ Game/Systems/RegionSystem.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <EntityComponentSystem/System.hpp>
+#include "../Components/Movable.h"
+#include "../Components/Region.h"
+#include <map>
+namespace Engine {
+
+	class RegionSystem : public EntityComponentSystem::System<RegionSystem> {
+	public:
+    RegionSystem() { }
+    void update();
+
+	private:
+    bool isInRegion(EntityComponentSystem::Entity const &movableEntity, EntityComponentSystem::Entity const &regionEntity) const;
+    int32_t findInMap(std::pair <uint64_t, uint64_t> p);
+    std::vector <std:: pair <uint64_t, uint64_t> > movableToRegion;
+	};
+
+}
